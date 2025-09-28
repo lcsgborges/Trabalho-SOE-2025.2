abbab331a6f448f1557e779283c1f6a9dda9bb88
locust <locusttheeater@gmail.com>
2025-09-27 23:22:50 -0300

    DOCS: adicionando datasheet do sensor
    Aparentemente, é uma das que precisamos.
    
    Também criando o script de tratamento de dados. O .csv vai entrar no gitignore por questões de tamanho, provavelmente.

---
+++ tratamento_de_dados/tratamento.py
@@ -0,0 +1,16 @@
+"""
+    Written by BCl0c
+
+    The expectative is that this is able to output 
+    a single clean .csv from the file provided.
+
+    This will be hardcoded for simplicity's sake.
+    Since most probably you'd need to do this 
+    differently for every dataset, there's 
+    no actual advantage to try to make this 
+    general. Pandas is already general enough.
+
+    Anyway, given a good dataset, we need to extract
+    temperature, humidity
+
+"""
