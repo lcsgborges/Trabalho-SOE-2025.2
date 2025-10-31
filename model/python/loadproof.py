a2f4d3210d8e033eea8798766844ccb5df45aa92
l0c <locusttheeater@gmail.com>
2025-10-31 14:27:26 -0300

    FIX: fixing the blasted files

---
+++ model/python/loadproof.py
@@ -0,0 +1,34 @@
+# written by l0c!
+
+# this script is a proof of concept on how to load correctly some data into the
+# model in a well behaved manner.
+
+# as this is a very small model, it will probably not consume much memory
+# overall, so we're not very worried about this.
+
+#initialization
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+MODELPATH = 't24v1.keras'
+TESTCSV   = '../tratamento_de_dados/dataset1.csv'
+
+#loadings
+model = tf.keras.model.load_model(MODELPATH)
+df = pd.read_csv(TARGET, parse_dates=[0], date_format="%Y-%m-%d")
+
+#next, we reformat data so the model understands and we may compare
+#what we got.
+
+inp = df['Temp. Ins. (C)'][1:49].to_numpy()
+inp.shape = (2,24)
+
+#get predictions
+predictions = model.predict(inp[0])
+
+counter = 0
+print("--------")
+for a,b in zip(predictions, inp[1]):
+	print("|", counter, "|", a, "|", b, "|")
+
+
