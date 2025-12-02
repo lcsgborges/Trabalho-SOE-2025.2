7d714e3bc7b5df73fc7b4bec31d3844395717184
lcsgborges <lcsgborges@gmail.com>
2025-12-02 20:24:28 -0300

    alteracoes ponto de controle 4

--- model/python/loadproof.py
+++ model/python/loadproof.py
@@ -10,24 +10,35 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
-MODELPATH = '../t24v1.keras'
-TESTCSV   = '../../tratamento_de_dados/dataset2.csv'
+MODELPATH = './t120v1.keras'
+TARGET    = './../tratamento_de_dados/dataset2.csv'
+CONV_WIDTH = 3
+OUT_STEPS = 120
+MAX_EPOCHS = 20
+INSTEMP   = 'TEMPERATURA DO AR - BULBO SECO, HORARIA (C)'
+MINTEMP   = 'TEMPERATURA MINIMA NA HORA ANT. (AUT) (C)'
+MAXTEMP   = 'TEMPERATURA MAXIMA NA HORA ANT. (AUT) (C)'
 
+def custom(x):
+	return x[:, -CONV_WIDTH:, : ]
 #loadings
-model = tf.keras.model.load_model(MODELPATH)
+tf.keras.config.enable_unsafe_deserialization()
+model = tf.keras.models.load_model(MODELPATH)
 df = pd.read_csv(TARGET, parse_dates=[0], date_format="%Y-%m-%d")
-
+print(df.info())
 #next, we reformat data so the model understands and we may compare
 #what we got.
 
 #normalize and reshape steps
-inp = df['Temp. Ins. (C)'][1:121].to_numpy()
+
+inp = df[[INSTEMP, MINTEMP, MAXTEMP]][1:121].to_numpy()
 mean = inp.mean()
 std = inp.std()
 inp = (inp - inp.mean())/ inp.std()
-npinp = inp.reshape(1,24,3)
+npinp = inp.reshape(1,120,3)
 
 #get predictions
+CONV_WIDTH = 3 #MAGIC NUMBER!
 predictions = model.predict(npinp)
 
 #denormalize
