a79edfa0fdcc6c33e7234d8fa513e83d36d0a324
l0c <locusttheeater@gmail.com>
2025-12-03 15:19:20 -0300

    adding image capabilities to loadproof

--- model/python/loadproof.py
+++ model/python/loadproof.py
@@ -10,8 +10,11 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
 MODELPATH = './t120v1.keras'
 TARGET    = './../tratamento_de_dados/dataset2.csv'
+TARGET2   = './mini.csv'
 CONV_WIDTH = 3
 OUT_STEPS = 120
 MAX_EPOCHS = 20
@@ -19,19 +22,50 @@ INSTEMP   = 'TEMPERATURA DO AR - BULBO SECO, HORARIA (C)'
 MINTEMP   = 'TEMPERATURA MINIMA NA HORA ANT. (AUT) (C)'
 MAXTEMP   = 'TEMPERATURA MAXIMA NA HORA ANT. (AUT) (C)'
 
+INSTEMP2   = 'ins'
+MINTEMP2   = 'min'
+MAXTEMP2   = 'max'
+
+FIGUREPATH = None
+
 def custom(x):
 	return x[:, -CONV_WIDTH:, : ]
+
+def gen_plot24(values, path="fig"):
+  #side effects: will change import name to plt, will change 
+  values = values.reshape(3, 24)
+  x1 = values[0]
+  x2 = values[1]
+  x3 = values[2]
+  plt.plot(x1)
+  plt.plot(x2)
+  plt.plot(x3)
+  plt.savefig(path)
+
+def gen_plot120(values):
+  #side effects: will change import name to plt, will change 
+  values = values.reshape(3, 120)
+  x1 = values[0]
+  x2 = values[1]
+  x3 = values[2]
+  plt.plot(x1)
+  plt.plot(x2)
+  plt.plot(x3)
+  plt.savefig(path)
+
 #loadings
 tf.keras.config.enable_unsafe_deserialization()
 model = tf.keras.models.load_model(MODELPATH)
-df = pd.read_csv(TARGET, parse_dates=[0], date_format="%Y-%m-%d")
+#df = pd.read_csv(TARGET, parse_dates=[0], date_format="%Y-%m-%d")
+df = pd.read_csv(TARGET2)
 print(df.info())
 #next, we reformat data so the model understands and we may compare
 #what we got.
 
 #normalize and reshape steps
 
-inp = df[[INSTEMP, MINTEMP, MAXTEMP]][1:121].to_numpy()
+#inp = df[[INSTEMP, MINTEMP, MAXTEMP]][1:121].to_numpy()
+inp = df[[INSTEMP2, MINTEMP2, MAXTEMP2]][1:121].to_numpy()
 mean = inp.mean()
 std = inp.std()
 inp = (inp - inp.mean())/ inp.std()
@@ -44,6 +78,10 @@ predictions = model.predict(npinp)
 #denormalize
 predictions = (predictions * mean) + std
 
-print(npinp)
-print(predictions)
+if(FIGUREPATH == None):
+	gen_plot120(predictions)
+else:
+	gen_plot120(predictions, FIGUREPATH)
+
+
 
